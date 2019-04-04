bcd6c3cf8c9e936bb023df9ce59370459dd3628e
sddrozd <sddrozd@A21COMP7>
2019-04-04 20:37:54 +0300

    Add project files.

---
+++ MyServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace MyServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient receiver = new UdpClient(8080);
+            IPEndPoint remoteIp = null;
+            Console.WriteLine("Start Server");
+
+            Picture();
+
+            while (true)
+            {
+                
+                byte[] data = receiver.Receive(ref remoteIp);
+                if (Encoding.Unicode.GetString(data) == "Send me a date")
+                {
+                    IPEndPoint ip = remoteIp as IPEndPoint;
+                    data = Encoding.Unicode.GetBytes(DateTime.Now.ToString());
+                    receiver.Send(data, data.Length, ip.Address.ToString(), ip.Port);
+                }
+            }
+        }
+
+        private static void Picture()
+        {
+            Graphics graph = null;
+            var bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            graph = Graphics.FromImage(bmp);
+
+            graph.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+
+            bmp.Save("filename.png");
+        }
+    }
+}
---
+++ Time sinh/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Time_sinh
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private async void SendClick(object sender, RoutedEventArgs e)
+        {
+            var s = await Task.Run(() => MyTimeParse());
+            TimeLable.Content = s;
+        }
+
+        private string MyTimeParse()
+        {
+
+            UdpClient sender = new UdpClient();
+
+            byte[] data = Encoding.Unicode.GetBytes("Send me a date");
+
+            sender.Send(data, data.Length, "127.0.0.1", 8080);
+
+            IPEndPoint remoteIp = null;
+            data = sender.Receive(ref remoteIp);
+            return Encoding.Unicode.GetString(data);
+        }
+    }
+}
+
